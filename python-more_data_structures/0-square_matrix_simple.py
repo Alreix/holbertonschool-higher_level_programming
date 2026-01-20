0520242149570d3af53547b9eb16655cb22bc65e
Alreix <alreixdev@gmail.com>
2026-01-20 10:09:46 +0100

    add(square_matrix): a function that return the square value of a matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for number in row:
+            square = number * number
+            new_row.append(square)
+        new_matrix.append(new_row)
+
+    return new_matrix
