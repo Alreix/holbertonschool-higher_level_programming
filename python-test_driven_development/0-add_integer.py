f8a387de8c5385d2b717dd1294b24f51c815e211
Alreix <alreixdev@gmail.com>
2026-01-21 14:26:33 +0100

    fix(add_int): reduce the size of docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-"""
-Module for the add_integer function.
-
-This module provides a function that adds two numbers
-and returns sum. The arguments must be
-integers or floats, otherwise a TypeError is raised.
+"""Module for add_integer.
+Defines one function add_integer(a, b=98).
+Adds two integers or floats, casting floats to ints.
+Raises a TypeError if either argument is not
+an integer or a float.
 """
 
 
 def add_integer(a, b=98):
-    """
-    Adds two numbers and returns an integer.
+    """Add two integers or floats.
 
-    The arguments a and b must be integers or floats.
-    If they are floats, they are first converted to integers.
-
-    Returns:
-        The integer sum of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(2)
-    100
+    Floats are cast to ints before addition.
+    Raise TypeError if a or b is not int or float.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
