6e88006a122f31d7e2b8edecde6389ed8f898b09
Alreix <alreixdev@gmail.com>
2026-03-03 15:32:39 +0100

    feat(create read): create a database and create a user with select privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,19 @@
+-- Creates database hbtn_0d_2 and user user_0d_2 with SELECT privilege only
+
+-- Create database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2@localhost if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply privilege changes
+FLUSH PRIVILEGES;
+
+
+
+
+
+
