8cc4f94a21e02847db07f0cfce7dd8543b026a2b
Alreix <alreixdev@gmail.com>
2026-03-03 14:29:16 +0100

    feat(privilege): a script that list all privileges for user1 and user2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Lists all privileges for MySQL users user_0d_1 and user_0d_2 on localhost
+
+-- Privileges for user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Privileges for user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
