1a9aa14a19dc23464d6eae5920b01ba9233f78d3
Alreix <alreixdev@gmail.com>
2026-03-04 16:04:25 +0100

    feat(my_filter_states): Display states where the name matches the user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Display states where the name matches the user input.
+
+Usage:
+    ./2-my_filter_states.py <user> <password> <database> <state_name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passeword = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passeword,
+        db=db_name
+    )
+    cur = db.cursor()
+
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    ).format(state_name_searched)
+
+    cur.execute(query)
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
