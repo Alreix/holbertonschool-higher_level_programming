f1cda8dc9eb4b0866219fb14403f7eadf31332a1
Alreix <alreixdev@gmail.com>
2026-03-04 17:39:43 +0100

    feat(model_state): remove change

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,5 +14,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
