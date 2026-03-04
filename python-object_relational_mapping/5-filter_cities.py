736bdaf5b88d08b3f8c8859007f4e1a186319b9d
Alreix <alreixdev@gmail.com>
2026-03-04 17:16:29 +0100

    feat(filter_cities): a script for list all cities of a given state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+List all cities of a given state from a MySQL database.
+
+The query is parameterized to prevent SQL injection.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
+    cur.execute(query, (state_name,))
+
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
