a95985c74124c1ccd26d76e62133c258bcaf653f
Alreix <alreixdev@gmail.com>
2026-03-04 15:53:37 +0100

    feat(filter_states): add list all states starting with 'N' from MySQL database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+"""
+List all states starting with 'N' from a MySQL database.
+
+Usage:
+    ./1-filter_states.py <user> <password> <database>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    "Retrive command argument"
+    user = sys.argv[1]
+    passeword = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passeword,
+        db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    )
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
