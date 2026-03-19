d2da5d62d7e27157254559955fa9e3293e5f94de
Alreix <alreixdev@gmail.com>
2026-03-19 10:34:12 +0100

    feat: adding a script that used process argv and conditional logic

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Node.js program to demonstrate the use of process.argv with if and else
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
