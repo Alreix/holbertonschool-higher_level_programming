3f6664269cea64d2c4332f0a3ce4c7819c6e8a97
Alreix <alreixdev@gmail.com>
2026-03-19 14:01:44 +0100

    fix: using isNan instead of Number verification

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-if (Number(process.argv[2])) {
-  const x = Number(process.argv[2]);
-  for (let i = 0; i <= x; i++) {
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
